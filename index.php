bf8594e297a03d771a35f7fb554da69daa6f0f40
dustytrash <Lionelbergen@live.com>
2021-01-13 21:33:19 -0600

    g

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<h1>PROTOCOL</h1>
+<?php
+$_SERVER['REQUEST_METHOD']
+?>
+
+<h1>REQUEST</h1>
+<?php
+var_dump($_GET); 
+?>
+
 <h1>HEADERS:</h1>
 <?php
 foreach (getallheaders() as $name => $value) {
@@ -5,4 +15,4 @@ foreach (getallheaders() as $name => $value) {
 }
 ?>
 
-<h1>navigator:</h1>
+<h1>Navigator:</h1>
