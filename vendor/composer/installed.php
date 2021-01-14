27447fa36a7d8dd05a4d06d6cbe8f5d8cd2c422c
dustytrash <Lionelbergen@live.com>
2021-01-13 20:46:03 -0600

    ran composer install

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '9baf2153f00f93ebfcc910ed4e61265a41a5b63c',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9baf2153f00f93ebfcc910ed4e61265a41a5b63c',
+    ),
+  ),
+);
